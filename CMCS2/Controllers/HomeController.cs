781089419e4a4f83d6b3fdb35ebe1eeeb3c4bc03
Makhubele, Fumi <fumimakh@gmail.com>
2025-10-22 17:39:17 +0200

    testing
    testing 1

--- CMCS2/Controllers/HomeController.cs
+++ CMCS2/Controllers/HomeController.cs
@@ -28,5 +28,5 @@ namespace CMCS2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-    }
+    }//ss
 }
